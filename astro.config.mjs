c47e8ae9607320d32b39375dfe4789df946b4172
Amado Martínez <amr89.dev@gmail.com>
2025-04-20 21:42:48 -0500

    Actualiza la configuración de despliegue en astro.yml, simplifica el proceso de instalación de dependencias y mejora la estructura del portafolio en index.astro. Se ajustan estilos en Contact.astro y Stack.astro para una mejor presentación.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://[tu-usuario].github.io',
+  base: '/portfolio',
+  output: 'static',
   integrations: [tailwind()]
 });
