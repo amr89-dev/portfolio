3f8e9c68e5fec50dd29b7d92ac4ac62bd689f277
Amado Martínez <amr89.dev@gmail.com>
2025-04-21 21:06:32 -0500

    Actualiza la configuración de Astro, eliminando parámetros innecesarios y simplificando la estructura del archivo. Se reemplaza el favicon por un nuevo diseño SVG y se ajusta el layout en Layout.astro para mejorar la presentación y la responsividad del portafolio.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://[tu-usuario].github.io',
-  base: '/portfolio',
-  output: 'static',
-  integrations: [tailwind()]
-});
+  integrations: [tailwind()],
+})
